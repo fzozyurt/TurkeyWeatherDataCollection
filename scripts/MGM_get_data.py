80dfec003740cea2dea02929242327843983d332
fzozyurt <fzozyurt@outlook.com>
2024-06-09 18:49:22 +0300

    Update

--- scripts/MGM_get_data.py
+++ scripts/MGM_get_data.py
@@ -27,30 +27,33 @@ ilceler.to_csv("data/Ilce_List.csv",sep=';',index=False,encoding="utf-8")
 havadurum = pd.read_csv("data/Hava_Durumu.csv", sep=";")
 
 for mid in ilceler["merkezId"]:
+    print(f'{mid} işlem başladı')
     havadurum_url = f"https://servis.mgm.gov.tr/web/tahminler/gunluk?istno={mid}"
-    response = requests.get(havadurum_url,headers=headers,verify=False)
-    data = response.json()[0]
-    rows = []
-    # 5 günlük veriyi satır satır ekleyeceğiz
-    for i in range(1, 6):
-        row = {
-            "istNo": str(data["istNo"]),
-            "tarih": data[f"tarihGun{i}"],
-            "enDusuk": str(data[f"enDusukGun{i}"]),
-            "enYuksek": data[f"enYuksekGun{i}"],
-            "hadise": data[f"hadiseGun{i}"],
-            "enDusukNem": data[f"enDusukNemGun{i}"],
-            "enYuksekNem": data[f"enYuksekNemGun{i}"],
-            "ruzgarYon": data[f"ruzgarYonGun{i}"],
-            "ruzgarHiz": data[f"ruzgarHizGun{i}"]
-        }
-        rows.append(row)
-    
-    havadurum = pd.concat([havadurum,pd.DataFrame(rows)])
+    response = requests.get(havadurum_url,headers=headers)
+    if response.json()!=[]:
+        data = response.json()[0]
+        rows = []
+        # 5 günlük veriyi satır satır ekleyeceğiz
+        for i in range(1, 6):
+            row = {
+                "istNo": str(data["istNo"]),
+                "tarih": data[f"tarihGun{i}"],
+                "enDusuk": str(data[f"enDusukGun{i}"]),
+                "enYuksek": data[f"enYuksekGun{i}"],
+                "hadise": data[f"hadiseGun{i}"],
+                "enDusukNem": data[f"enDusukNemGun{i}"],
+                "enYuksekNem": data[f"enYuksekNemGun{i}"],
+                "ruzgarYon": data[f"ruzgarYonGun{i}"],
+                "ruzgarHiz": data[f"ruzgarHizGun{i}"]
+            }
+            rows.append(row)
+        
+        havadurum = pd.concat([havadurum,pd.DataFrame(rows)])
+        print(f'{mid} işlem bitti')
+    else:
+        print(f'{mid} Null geldiğinden atlandı')
 
 havadurum=havadurum.drop_duplicates()
 havadurum.reset_index(inplace=True)
 havadurum.drop("index",axis=1,inplace=True)
 havadurum.to_csv("data/Hava_Durumu.csv",sep=';',index=False,encoding="utf-8")
-
-
